2f379c542f97a6fd467f5885236a3ec6d00fb90d
Jin Liu <iamjincn@outlook.com>
2025-06-16 11:19:43 +1000

    Add initial project structure and core application files for HBnB API

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -16,18 +16,3 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
-
-class HBnBFacade:
-    def __init__(self):
-        self.user_repo = InMemoryRepository()
-
-    def create_user(self, user_data):
-        user = User(**user_data)
-        self.user_repo.add(user)
-        return user
-
-    def get_user(self, user_id):
-        return self.user_repo.get(user_id)
-
-    def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute('email', email)
